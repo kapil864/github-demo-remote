a51fdebecacf74045f94fb8f81e3f725eb222f60
Kapil Kumar <69892172+kapil864@users.noreply.github.com>
2022-01-21 11:05:35 +0530

    initial commit of helloWorld

---
+++ helloWorld.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello world"<<endl;
+	// add subtraction
+	cout<<"implemented subtract function in master barnch"<<endl;
+	// add addition
+	cout<<"Implemented addition function on bug456 branch"<<endl;
+	// add multipication
+	if (1==1)
+		cout<<4*5;
+	else 
+		cout<<8*9;
+	// add division
+
+	// sqaure implementation
+	cout<<"Sqare fuction"<<endl; // to demonstrate stashing
+	return 0;	
+}
